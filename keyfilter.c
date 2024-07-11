6cb24aa09a5a4b2f866c34d2e286b6fe49efe4ce
Andrii <and.klimenkoo@gmail.com>
2024-07-11 23:32:32 +0200

    added function that checks if the input argument has a valid length

--- keyfilter.c
+++ keyfilter.c
@@ -1,8 +1,16 @@
 #include <stdbool.h>
+#include <string.h>
+
+#define MAX_ADDRESS_LENGTH 100
 
 bool isValidArgc(int argc)
 {
     return argc <= 1;
+}
+
+bool isValidArgLength(char* arg)
+{
+    return strlen(arg) <= MAX_ADDRESS_LENGTH;
 }
 
 int main()
