95e26856b11fee3673869900f212e4dae9c1c60c
Andrii <and.klimenkoo@gmail.com>
2024-07-12 17:15:49 +0200

    modified main function: now program checks its arguments and is ready to manipulate with it

--- keyfilter.c
+++ keyfilter.c
@@ -1,7 +1,10 @@
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_ADDRESS_LENGTH 100
+#define MAX_ADDRESS_BUFFER_LENGTH (MAX_ADDRESS_LENGTH + 1) // + '\0'
 
 bool isValidArgCount(int arg_count)
 {
@@ -20,5 +23,17 @@ bool isValidArg(int arg_count, char* arg)
 
 int main(int argc, char** argv)
 {
-    return 0;
+    if(!isValidArg(argc - 1, argv[1]))
+    {
+        fprintf(stderr, "Error! Wrong number of program arguments (expected 0 or 1) or invalid length of first argument.\n");
+        return EXIT_FAILURE;
+    }
+    
+    char searched_address[MAX_ADDRESS_BUFFER_LENGTH] = {'\0', };
+    if(argc - 1 != 0)
+    {
+        strcpy(searched_address, argv[1]);
+    }
+    
+    return EXIT_SUCCESS;
 }
